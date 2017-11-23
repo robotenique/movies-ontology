c1dff0f0b5a7013ca3a9d5918835cc6234f2f925
robotenique <julianogarcia_1997@hotmail.com>
2017-11-23 11:22:33 -0200

    modifications to the imdbAnalyzer script

--- imdbAnalyzer.py
+++ imdbAnalyzer.py
@@ -1,32 +1,39 @@
 import re
 class Act(object):
-    def __init__(self, f, l):
-        self.f = f
-        self.l = l
-    def nlist(self):
-        return [self.f, self.l]
+    def wsearch(w):
+        return re.compile(r'\b({0})\b'.format(w), flags=re.IGNORECASE).search
+
+    def __init__(self, first, last):
+        self.f = first
+        self.l = last
+
+    def regexp(self):
+        return (Act.wsearch(self.f), Act.wsearch(self.l))
+
     def __repr__(self):
         return f"f: {self.f} l: {self.l}"
 
 ## Finding Actors Information!
-act_l = ["Uma Thurman", "Harvey Keitel", "Bill Murray", "Frances McDormand"]
-act_man_f = "actors.list"
-act_woman_f = "t"
-act_l = [(lambda n: Act(n[0], n[1]))([t.lower() for t in s.split()]) for s in act_l]
+act_n = ["Uma Thurman", "Harvey Keitel", "Bill Murray", "Frances McDormand"]
+#act_man_f = "actors.list"
+#act_woman_f = "actresses.list"
+act_man_f = "t"
+act_woman_f = "teste"
+act_l = [(lambda n: Act(n[0], n[1]))([t.lower() for t in s.split()]) for s in act_n]
 act_d = {}
 
 
 def find_actor_line(l, f):
     if l and l[0] != '\t':
         for i, a in enumerate(act_l):
-            if all(x in l.lower().replace(",","") for x in (a.f, a.l)) and not act_d.get(i, None):
-                act_d[i] = []
+            if all(x(l.lower().replace(",","")) for x in a.regexp()) and not act_d.get(act_n[i], None):
+                act_d[act_n[i]] = []
                 cs = [l[l.find('\t'):].replace("\t","")]
                 l = f.readline()
                 while l and l[0] == '\t':
                     cs.append(l.replace("\t", ""))
                     l = f.readline()
-                act_d[i].append("".join(cs))
+                act_d[act_n[i]].append("".join(cs))
     return l
 
 
@@ -36,9 +43,9 @@ with open(act_man_f, "r", encoding='latin-1') as m, open(act_woman_f, "r", encod
     lw = w.readline()
     while lm or lw:
         lm, lw = find_actor_line(lm, m), find_actor_line(lw, w)
-        lm = m.readline(),  w.readline()
-        if c%10000 == 0:
-            print(f"Lendo {c+1}º linha...")
+        lm, lw = m.readline(),  w.readline()
+        if c%50000 == 0:
+            print(f"{c}º iteration...")
         c+=1
 
 print(act_d)
